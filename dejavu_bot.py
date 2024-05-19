8e3731c55f954a1127986062c4dd8d451b6d302b
aaronson2012 <15083264+aaronson2012@users.noreply.github.com>
2024-05-19 05:12:22 -0500

    Update dejavu_bot.py

--- dejavu_bot.py
+++ dejavu_bot.py
@@ -131,8 +131,10 @@ async def who_said(who_said_id, who_said_content, channel):
 
 @bot.listen
 async def on_message(message):
+    print('on_message called')
     # this if statement only returns true if who_said has run before this
     if message.mentions.id == who_said_id_for_on_message:
+        print('message id is equal to whosaid response id')
         channel.send('Correct.')
         who_said_id_for_on_message = ''
 
