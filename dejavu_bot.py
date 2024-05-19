026d61b4a4213f81b13508e4dc3ddc251b0837f2
Collin MacDonald <macd.collin@gmail.com>
2024-05-18 23:22:57 -0500

    Change message to bot command

--- dejavu_bot.py
+++ dejavu_bot.py
@@ -13,6 +13,7 @@ from random import choice, randrange
 from PIL import Image, ImageColor, ImageDraw, ImageFont
 
 import discord
+from discord.ext import commands
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -21,6 +22,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 VERY_DARK_COLORS = [
     'black',
@@ -40,24 +42,19 @@ async def on_ready():
     """
     print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
+@bot.command()
+async def dejavu(ctx, arg):
     """
-    This will run on every message...
-    However, if the message is `/dejavu` we'll do stuff
+    On `/dejavu` grab a random message and post it
     """
-    if message.author == client.user or not message.content.startswith('/dejavu'):
-        # Bot cannot reply to itself
-        return
-
-    channel = client.get_channel(os.environ.get('CHANNEL_ID'))
+    channel = ctx.channel
     created_at = channel.created_at
     rand_datetime = get_rand_datetime(created_at, datetime.now(tzinfo=timezone.utc))
 
     rand_message = channel.history(limit=1, around=rand_datetime)[0]
 
     if rand_message.content != '':
-        create_and_send_image(rand_message, channel)
+        create_and_send_response(rand_message, channel, arg)
 
 def get_rand_datetime(start, end):
     """
@@ -71,31 +68,43 @@ def get_rand_datetime(start, end):
     random_second = randrange(int_delta)
     return start + datetime.timedelta(seconds=random_second)
 
-async def create_and_send_image(rand_message, channel):
+def create_and_send_response(rand_message, channel, arg):
     """
     Creates an image with the message author, content, and creation datetime
     with a random colored background
     """
-    width = 100
-    height = 100
     font = ImageFont.truetype("FreeMono.ttf", size=20)
     text = (
         rand_message.author +
-        " said: " +
+        " said: \n" +
         rand_message.content +
-        " at " +
+        "\n at " +
         str(rand_message.created_at)
     )
 
+    # if the arg is "text" just return the text as a message
+    if arg == 'text':
+        # handle the case where text is requested
+        # /dejavu text
+        channel.send(text)
+    elif arg == 'image':
+        create_and_send_image(100, text, font, channel)
+
+async def create_and_send_image(width_height, text, font, channel):
+    """
+    Handle the case where an image is requested
+    /dejavu image
+    """
+
     # the second element is the color name
     rand_color = choice(ImageColor.colormap.items())[0]
-    img = Image.new('RGB', (width, height), color=rand_color)
+    img = Image.new('RGB', (width_height, width_height), color=rand_color)
 
     img_draw = ImageDraw.Draw(img)
 
     text_width, text_height = img_draw.textsize(text, font=font)
-    x_text = (width - text_width) / 2
-    y_text = (height - text_height) / 2
+    x_text = (width_height - text_width) / 2
+    y_text = (width_height - text_height) / 2
 
     if rand_color not in VERY_DARK_COLORS:
         img_draw.text((x_text, y_text), text, font=font, fill=(0, 0, 0))
