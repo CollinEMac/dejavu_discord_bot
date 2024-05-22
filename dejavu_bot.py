286065e59760a29d632f4cf4f56661e7a7848fc5
aaronson2012 <15083264+aaronson2012@users.noreply.github.com>
2024-05-22 14:40:34 -0500

    Update dejavu_bot.py

--- dejavu_bot.py
+++ dejavu_bot.py
@@ -142,7 +142,7 @@ async def who_said(who_said_message, channel):
     Set who_said_playing to true so the if statement in on_message gets triggered
     """
     bot.who_said_playing = True
-    bot.who_said_author = who_said_message.author
+    bot.who_said_author = who_said_message.author.name
     await channel.send('Who said: ' + who_said_message.content)
 
 @bot.event
@@ -155,7 +155,7 @@ async def on_message(message):
         return
 
     # this if statement only returns true if who_said has run before this
-    if len(message.mentions) > 0 and message.mentions[0].id == bot.who_said_author.id:
+    if len(message.mentions) > 0 and message.mentions[0].name == bot.who_said_author:
         await message.reply('Correct.')
         bot.who_said_author = None
         bot.who_said_second_chance = True
@@ -164,7 +164,7 @@ async def on_message(message):
         await message.reply('Wrong! I\'ll give you one more chance.')
         bot.who_said_second_chance = False
     elif bot.who_said_second_chance == False:
-        await message.reply('Wrong again! It was ' + bot.who_said_author.name + '! Game over!.')
+        await message.reply('Wrong again! It was ' + bot.who_said_author + '! Game over!.')
         bot.who_said_author = None
         bot.who_said_second_chance = True
         bot.who_said_playing = False
