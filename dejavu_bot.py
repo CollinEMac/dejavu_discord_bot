80307ea405e8ceeb6f116838ad508bbf865f64b6
aaronson2012 <15083264+aaronson2012@users.noreply.github.com>
2024-05-22 15:50:08 -0500

    beautify

--- dejavu_bot.py
+++ dejavu_bot.py
@@ -19,6 +19,7 @@ from discord import app_commands
 
 # Load .env file
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Set up bot
@@ -31,48 +32,55 @@ tree = app_commands.CommandTree(bot)
 
 # Colors for the image mode.
 VERY_DARK_COLORS = [
-    'black',
-    'darkblue',
-    'darkmagenta',
-    'darkslategrey',
-    'indigo',
-    'midnightblue',
-    'navy',
-    'purple'
+    "black",
+    "darkblue",
+    "darkmagenta",
+    "darkslategrey",
+    "indigo",
+    "midnightblue",
+    "navy",
+    "purple",
 ]
 
 # Initalize whosaid game variables
 bot.whosaid = {}
-bot.whosaid['playing'] = False
-bot.whosaid['author'] = None
-bot.whosaid['message'] = ''
-bot.whosaid['second_chance'] = False
+bot.whosaid["playing"] = False
+bot.whosaid["author"] = None
+bot.whosaid["message"] = ""
+bot.whosaid["second_chance"] = False
 
 # Command setup and argument definition.
 @tree.command(
     name="dejavu",
     description="Devjavu bot",
 )
-@app_commands.choices(choices=[
-    app_commands.Choice(name="Retrieve a random message.", value="text"),
-    app_commands.Choice(name="Retrieve a random message and put it in an image.", value="image"),
-    app_commands.Choice(name="Retrieve a random message and you must guess who said it by mentioning them.", value="whosaid"),
-    ])
+@app_commands.choices(
+    choices=[
+        app_commands.Choice(name="Retrieve a random message.", value="text"),
+        app_commands.Choice(
+            name="Retrieve a random message and put it in an image.", value="image"
+        ),
+        app_commands.Choice(
+            name="Retrieve a random message and you must guess who said it by mentioning them.",
+            value="whosaid",
+        ),
+    ]
+)
 async def dejavu(inter, choices: app_commands.Choice[str]):
     """
     On `/dejavu text|image|whosaid` grab a random message and post it in the chosen format.
     """
 
     # If a game of whosaid is already being played, do not continue.
-    if bot.whosaid['playing'] == True:
-        await inter.response.send_message('I\'m still waiting for you to guess.')
+    if bot.whosaid["playing"] == True:
+        await inter.response.send_message("I'm still waiting for you to guess.")
         return
 
     # Send a message to confirm recipt of command.
     # Else the bot will error.
     # TODO: Find a way to run a command without
     # something like this
-    await inter.response.send_message('Command sent.')    
+    await inter.response.send_message("Command sent.")
 
     # Get a random datetime between now and when the channel was created
     channel = inter.channel
@@ -83,9 +91,10 @@ async def dejavu(inter, choices: app_commands.Choice[str]):
     # Fetch a message using the random datetime
     # limit=1 so we only get one message (we could change this later to add more?)
     async for rand_message in channel.history(limit=1, around=rand_datetime):
-        if rand_message.content != '':
+        if rand_message.content != "":
             await create_and_send_response(rand_message, channel, choices.value)
             break
+
 
 def get_rand_datetime(start, end):
     """
@@ -98,28 +107,30 @@ def get_rand_datetime(start, end):
     random_second = randrange(int_delta)
     return start + timedelta(seconds=random_second)
 
+
 async def create_and_send_response(rand_message, channel, choice):
 
     text = (
-        rand_message.author.name +
-        " said: \n" +
-        rand_message.content +
-        "\nat " +
-        rand_message.created_at.strftime("%Y-%m-%d %I:%M %p")
+        rand_message.author.name
+        + " said: \n"
+        + rand_message.content
+        + "\nat "
+        + rand_message.created_at.strftime("%Y-%m-%d %I:%M %p")
     )
 
-    if choice == 'text':
-        # Sends the random message with the message author, content, 
-        #and creation datetime
+    if choice == "text":
+        # Sends the random message with the message author, content,
+        # and creation datetime
         # `/dejavu text`
         await channel.send(text)
-    elif choice == 'image':
+    elif choice == "image":
         await create_and_send_image(text, channel)
-    elif choice == 'whosaid':
+    elif choice == "whosaid":
         # if the choice is whosaid, pass the rand_message and channel to whosaid()
         await whosaid(rand_message, channel)
     else:
-        await channel.send('Invalid Command.')
+        await channel.send("Invalid Command.")
+
 
 async def create_and_send_image(text, channel):
     """
@@ -127,17 +138,14 @@ async def create_and_send_image(text, channel):
     with a random colored background. TODO: Doesn't work right.
     `/dejavu image`
     """
-    font = ImageFont.truetype(
-        "./fonts/Courier.ttf", # debian path
-        size=14
-    )
+    font = ImageFont.truetype("./fonts/Courier.ttf", size=14)  # debian path
 
     # Convert dict_items to a list
     color_items = list(ImageColor.colormap.items())
-    
+
     rand_color_name = choices(list(ImageColor.colormap.keys()))[0]
     rand_color = ImageColor.getrgb(rand_color_name)
-    img = Image.new('RGB', (1000, 100), color=rand_color)
+    img = Image.new("RGB", (1000, 100), color=rand_color)
 
     img_draw = ImageDraw.Draw(img)
 
@@ -148,11 +156,12 @@ async def create_and_send_image(text, channel):
 
     # Save the image to a BytesIO buffer
     buffer = BytesIO()
-    img.save(buffer, 'png')
+    img.save(buffer, "png")
     buffer.seek(0)  # Reset the buffer pointer to the beginning
 
-    file = discord.File(buffer, filename='image.png')
+    file = discord.File(buffer, filename="image.png")
     await channel.send(file=file)
+
 
 async def whosaid(message, channel):
     """
@@ -162,11 +171,12 @@ async def whosaid(message, channel):
     """
 
     # Set inital game variables and start the game
-    bot.whosaid['playing'] = True
-    bot.whosaid['channel'] = message.channel.id
-    bot.whosaid['second_chance'] = True
-    bot.whosaid['author'] = message.author.name
-    await channel.send('Who said: ' + message.content)
+    bot.whosaid["playing"] = True
+    bot.whosaid["channel"] = message.channel.id
+    bot.whosaid["second_chance"] = True
+    bot.whosaid["author"] = message.author.name
+    await channel.send("Who said: " + message.content)
+
 
 @bot.event
 async def on_message(message):
@@ -178,29 +188,37 @@ async def on_message(message):
 
     Then, the game logic runs.
     """
-    
+
     if message.author.bot == True:
         return
-    elif bot.whosaid['channel'] != message.channel.id:
+    elif bot.whosaid["channel"] != message.channel.id:
         return
 
     # This if statement only returns true if whosaid() has run before this
-    if len(message.mentions) > 0 and message.mentions[0].name == bot.whosaid['author'] and bot.whosaid['playing'] == True:
-        await message.reply('Correct.')
-        bot.whosaid['playing'] = False
-        bot.whosaid['second_chance'] = True
-    elif bot.whosaid['playing'] == True and bot.whosaid['second_chance'] == True:
-        await message.reply('Wrong! I\'ll give you one more chance.')
-        bot.whosaid['second_chance'] = False
-    elif bot.whosaid['playing'] == True and bot.whosaid['second_chance'] == False:
-        await message.reply('Wrong again! It was ' + bot.whosaid['author'] + '! Game over!.')
-        bot.whosaid['playing'] = False
-        bot.whosaid['second_chance'] = True
+    if (
+        len(message.mentions) > 0
+        and message.mentions[0].name == bot.whosaid["author"]
+        and bot.whosaid["playing"] == True
+    ):
+        await message.reply("Correct.")
+        bot.whosaid["playing"] = False
+        bot.whosaid["second_chance"] = True
+    elif bot.whosaid["playing"] == True and bot.whosaid["second_chance"] == True:
+        await message.reply("Wrong! I'll give you one more chance.")
+        bot.whosaid["second_chance"] = False
+    elif bot.whosaid["playing"] == True and bot.whosaid["second_chance"] == False:
+        await message.reply(
+            "Wrong again! It was " + bot.whosaid["author"] + "! Game over!."
+        )
+        bot.whosaid["playing"] = False
+        bot.whosaid["second_chance"] = True
+
 
 # Sync slash command to Discord.
 @bot.event
 async def on_ready():
     await tree.sync()
 
+
 # Run bot and load the Discord bot token from a `.env` file.
-bot.run(os.environ.get('DISCORD_TOKEN'))
+bot.run(os.environ.get("DISCORD_TOKEN"))
