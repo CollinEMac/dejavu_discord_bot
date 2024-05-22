e34e474516c3e1a6117ab53b7c27e1abfb9fea93
aaronson2012 <15083264+aaronson2012@users.noreply.github.com>
2024-05-22 09:48:54 -0500

    ctx is type interaction?

--- dejavu_bot.py
+++ dejavu_bot.py
@@ -48,7 +48,7 @@ async def dejavu(ctx: discord.Interaction, arg: str):
     On `/dejavu` grab a random message and post it
     """
 
-    await ctx.interaction.response.send_message('Command sent.')    
+    await ctx.response.send_message('Command sent.')    
     
     channel = ctx.channel
     created_at = channel.created_at
