1ef67950e2c13f3c74eafc79ee9e9983c4920de2
aaronson2012 <15083264+aaronson2012@users.noreply.github.com>
2024-05-22 11:17:50 -0500

    fix message assignment

--- dejavu_bot.py
+++ dejavu_bot.py
@@ -50,7 +50,7 @@ async def dejavu(ctx, arg: str):
     
     await ctx.response.send_message('Command sent.')    
 
-    message = ctx.fetch_message()
+    message = ctx.message.id
     created_at = channel.created_at
     end = datetime.utcnow().replace(tzinfo=timezone.utc)
     rand_datetime = get_rand_datetime(created_at, end)
