5add66cf172c9beb31120f4703e0dff4285d6825
aaronson2012 <15083264+aaronson2012@users.noreply.github.com>
2024-05-22 13:50:43 -0500

    Update dejavu_bot.py

--- dejavu_bot.py
+++ dejavu_bot.py
@@ -37,7 +37,7 @@ VERY_DARK_COLORS = [
 ]
 
 bot.who_said_playing = False
-bot.who_said_playing_attempts = 2
+bot.who_said_attempts = 2
 bot.who_said_user = None
 
 @tree.command(
@@ -157,14 +157,14 @@ async def on_message(message):
     # this if statement only returns true if who_said has run before this
     if len(message.mentions) > 0 and bot.who_said_playing == True and bot.who_said_attempts > 0:
         await message.reply('Correct.')
-        bot.who_said_playing = 1
+        bot.who_said_attempts = 1
     elif bot.who_said_playing == True:
         await message.reply('Wrong! I\'ll give you one more chance.')
-        bot.who_said_playing = 0
+        bot.who_said_attempts = 0
     else:
         await message.reply('Wrong again! It was ' + bot.who_said_user + '! Game over!.')
         bot.who_said_playing = False
-        bot.who_said_playing = 2
+        bot.who_said_attempts = 2
 
 # Sync slash command to Discord
 @bot.event
